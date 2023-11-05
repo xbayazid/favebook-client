f971809496adabbe44ab0b156b83fdd034dab4b5
xbayazid <bayazid9006@gmail.com>
2023-11-05 15:05:45 +0600

    sign up page email & password authentication completed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../../styles.css';
 import FillButton from '../../components/FillButton/FillButton';
 import ButtonUnFill from '../../components/ButtonUnFill/ButtonUnFill';
+import { useForm } from 'react-hook-form';
+import { AuthContext } from '../../context/AuthProvider';
 
 const Login = () => {
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const {signIn} = useContext(AuthContext);
+
+  const handleLogin = data => {
+    console.log(data);
+    
+  }
   return (
     <div>
       <div>
         <p className="Text">Please Login</p>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="name">
-            Email Address
-          </label>
-          <input type='email' className="inputs" id="email" defaultValue="" />
-        </fieldset>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="username">
-            Password
-          </label>
-          <input type='password' className="inputs" id="password" defaultValue="" />
-        </fieldset>
-        <div style={{ display: 'flex', marginTop: 20, justifyContent: 'center' }}>
-          <FillButton><input className='uppercase' type="submit" value="login" /></FillButton>
-        </div>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-control w-full">
+            <label className="label"><span className="label-text">Email</span></label>
+            <input {...register("email", { required: "Email Address is required" })} defaultValue="admin@gmail.com" type="text" className="input input-bordered w-full" />
+            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+          </div>
+          <div className="form-control w-full">
+            <label className="label"><span className="label-text">Password</span></label>
+            <input defaultValue="bayazid" {...register("password", { required: "Password is required", minLength: { value: 6, message: 'Password must be 6 characters or longer' } })} type="password" className="input input-bordered w-full" />
+            {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+            {/* <label className="label"><span className="label-text">Forget Password?</span></label> */}
+          </div>
+          <input className='btn btn-accent w-full my-3' value="Login" type="submit" />
+          {/* {loginError && <p className='text-red-600'>{loginError}</p>} */}
+        </form>
         <div className="divider">OR</div>
         <div className='text-center uppercase'>
           <ButtonUnFill>connect with google</ButtonUnFill>
