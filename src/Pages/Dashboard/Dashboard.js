17bd023cb5efa6b419597266763106059f5e0d2a
xbayazid <bayazid9006@gmail.com>
2023-12-24 14:17:41 +0600

    my post page designed completed

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,6 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/AuthProvider';
 import PostCard from '../../components/PostCard/PostCard';
+import { Link } from 'react-router-dom';
+import ButtonUnFill from '../../components/ButtonUnFill/ButtonUnFill';
+import { FaBookOpen } from "react-icons/fa";
+import { FaUserClock, FaUsers } from 'react-icons/fa6';
+
 
 const Dashboard = () => {
     const {user} = useContext(AuthContext);
@@ -19,6 +24,56 @@ const Dashboard = () => {
             {
                 posts.map(post => <PostCard key={post._id} postItem={post}/>)
             }
+
+            <div className='my-5 mx-3 bg-gray-300 p-3 rounded-xl'>
+                <div className='grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-7'>
+                    <Link to=''>
+                    <img src="https://i.ibb.co/JWZ8WjT/37d8de4ea3d351be88e2933c43d7ba49.jpg" alt="" />
+                    </Link>
+                    <Link to=''>
+                    <img src="https://i.ibb.co/JWZ8WjT/37d8de4ea3d351be88e2933c43d7ba49.jpg" alt="" />
+                    </Link>
+                    <Link to=''>
+                    <img src="https://i.ibb.co/JWZ8WjT/37d8de4ea3d351be88e2933c43d7ba49.jpg" alt="" />
+                    </Link>
+                    <Link to=''>
+                    <img src="https://i.ibb.co/JWZ8WjT/37d8de4ea3d351be88e2933c43d7ba49.jpg" alt="" />
+                    </Link>
+                    <Link to=''>
+                    <img src="https://i.ibb.co/JWZ8WjT/37d8de4ea3d351be88e2933c43d7ba49.jpg" alt="" />
+                    </Link>
+                    <Link to=''>
+                    <img src="https://i.ibb.co/JWZ8WjT/37d8de4ea3d351be88e2933c43d7ba49.jpg" alt="" />
+                    </Link>
+                    <Link to=''>
+                    <img src="https://i.ibb.co/JWZ8WjT/37d8de4ea3d351be88e2933c43d7ba49.jpg" alt="" />
+                    </Link>
+                </div>
+                <div className='w-1/2 mx-auto my-3'>
+                    <Link to='/book'>
+                    <ButtonUnFill>Recommanded Book for You</ButtonUnFill>
+                 </Link>
+                </div>
+            </div>
+            <div className="my-5 mx-3 bg-blue-300 px-3 py-6 rounded-xl">
+                <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ml-[10%] items-center">
+                    <div className='justify-center'>
+                    <FaBookOpen className='text-3xl lg:text-4xl' />
+                    <p>All Post</p>
+                    <h2 className='font-semibold text-2xl'>300</h2>
+                    </div>
+                    <div>
+                    <FaUsers className='text-4xl' />
+                    <p>All club</p>
+                    <h2 className='font-semibold text-2xl'>450</h2>
+                    </div>
+                    <div>
+                    <FaUserClock className='text-4xl' />
+                    <p>Appointment</p>
+                    <h2 className="font-semibold text-2xl">45</h2> 
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
